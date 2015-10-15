5c9d3b8067b79f149822a4a8883fc3a7e6485822
Jessie Leung <jleung@thoughtworks.com>
2015-10-15 16:55:34 +1100

    Refactoring conditionals

--- Prime-Factors/src/PrimeFactorsCalculator.js
+++ Prime-Factors/src/PrimeFactorsCalculator.js
@@ -2,10 +2,8 @@ function PrimeFactorsCalculator() {
   this.getPrimeFactors = function(number) {
     if (number === 1) {
       return [];
-    } else if (number == 2) {
-      return [2];
-    } else if (number == 3) {
-      return [3];
+    } else {
+      return [number];
     }
   }
 }
