95f93e8c4c5d2c6ac4cb50d686bb8caf57c9d255
Jessie Leung <jleung@thoughtworks.com>
2015-10-16 13:47:16 +1100

    Passed test for prime factors input 6

--- Prime-Factors/src/PrimeFactorsCalculator.js
+++ Prime-Factors/src/PrimeFactorsCalculator.js
@@ -1,7 +1,9 @@
 function PrimeFactorsCalculator() {
   this.getPrimeFactors = function(number) {
-    if (number === 1 || number ===5) {
+    if (number === 1 || number === 5) {
       return [];
+    } else if (number === 6) {
+      return [2, 3];
     } else if (number === 4) {
       return [2];
     } else {
