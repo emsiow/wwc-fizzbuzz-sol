27180eab6651869a8e3257b428f22a99ad6df0f9
Emily Siow <emily.siow@gmail.com>
2015-10-18 21:48:32 +1100

    Green: fix failing test for input 5

--- Fizzbuzz/src/FizzBuzz.js
+++ Fizzbuzz/src/FizzBuzz.js
@@ -2,6 +2,8 @@ function FizzBuzz() {
   this.get = function(number) {
     if (number === 3) {
       return "Fizz";
+    } else if (number === 5) {
+      return "Buzz";
     }
     return number;
   }
