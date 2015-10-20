ad593321a5c04a98131ff9413685d16e92237ffd
Jessie Leung <jleung@thoughtworks.com>
2015-10-20 22:32:57 +1100

    Added testing coverage

---
+++ Fizzbuzz/my.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'src/*.js',
+      'spec/*.js'
+    ],
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    plugins: ['karma-jasmine', 'karma-coverage', 'karma-phantomjs-launcher'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: { '*.js': ['coverage'] }
+  });
+};
